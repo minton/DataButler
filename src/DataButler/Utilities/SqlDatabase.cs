38d4f29c24db33623ed34dbc11eb0816689a4f3e
Michael Minton <michaelcminton@gmail.com>
2015-02-03 14:38:28 -0500

    Introduce SqlDatabase dto

---
+++ src/DataButler/Utilities/SqlDatabase.cs
@@ -0,0 +1,20 @@
+﻿namespace DataButler.Utilities
+{
+    public class SqlDatabase
+    {
+        public SqlDatabase(){}
+
+        public SqlDatabase(string name, string lastBackupName)
+        {
+            Name = name;
+            LastBackupName = lastBackupName;
+        }
+
+        public string Name { get; private set; }
+        public string LastBackupName { get; set; }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
